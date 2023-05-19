127bc41c27e811844449126d327079c182d1ead3
Nazli <nazlibulbuloglu@outlook.com>
2023-05-19 10:37:00 +0300

    tamamlandi

--- src/components/Kareler.js
+++ src/components/Kareler.js
@@ -13,54 +13,71 @@ Herhangi bir noktada yalnızca bir kare aktif olabilir (ya da hiçbiri)
 
 Aşaıdaki yorumları takip edin.
 */
-
-import React from 'react';
-
+import React from "react";
+import { useState } from "react";
 
 //Bu değişkeni YALNIZCA bir durum dilimini yüklemek için kullanın!
-const KareIdListesi = ['sqA', 'sqB', 'sqC', 'sqD'];
-
-
+const KareIdListesi = ["sqA", "sqB", "sqC", "sqD"];
 
 export default function Kareler() {
   // State hookunu 2 defa kullanın: 'kareler' ve
   // 'aktifKare' olmak üzere. Birisi kare idlerini _dizi_ olarak tutacak, diğeri ise aktif olan
   // kareyi gözlemleyecek. Sayfa yüklendiğinde aktif kare olmayacak,
   // yani  'aktifKare' null olmalı.
-	
-  const ClassAdiAl = id => {
+  const [kareler, setKareler] = useState(["sqA", "sqB", "sqC", "sqD"]);
+  const [aktifKare, setAktifKare] = useState(null);
+
+  const ClassAdiAl = (id) => {
     // Bu bir click handler değildir, JSX içinde kullanılan bir yardımcıdır(helper).(aşağıya bakın)
     // Eğer argüman olarak verilen id aktif kare state'indeki id ile eşleşirse, class adı 'active' olan bir string döndürecek
     // diğer durumlar için boş döndürecek.
     // Etkisini görmek için kareye sağ tıklayın ve "öğeyi inceleyin".
-	return ''
+    return aktifKare == id ? "active" : ""; //
   };
 
-  const AktifEt = id => {
+  const AktifEt = (id) => {
     // Bu bir _satır içinden çağırılmış_ click handler yardımcısıdır.
     // id bağımsız değişkenini, stateteki aktif id olacak şekilde ayarlayın
     // eğer zaten aktifse, o zaman önce state i resetlemeliyiz.
+    //setAktifKare(id === aktifKare ? null : id);
+
+    setAktifKare((prevAktifKare) => {
+      return prevAktifKare === id ? null : id;
+    });
+  };
+
+  const yeniKareEkle = () => {
+    // Yeni bir kare eklemek için, mevcut kareler dizisini kopyalayın ve yeni kareyi ekleyin.
+    setKareler((prevKareler) => {
+      const yeniKareler = [...prevKareler, `sq${prevKareler.length + 1}`];
+      return yeniKareler;
+    });
+  };
+
+  const kareSil = () => {
+    // Aktif kareyi silmek için, mevcut kareler dizisini kopyalayın ve aktif kareyi hariç tutun.
+    setKareler((prevKareler) => {
+      const yeniKareler = prevKareler.filter((id) => id !== aktifKare);
+      return yeniKareler;
+    });
+    setAktifKare(null); // Aktif kareyi null olarak sıfırlayın.
   };
 
   return (
-    <div className='widget-squares container'>
+    <div className="widget-squares container">
       <h2>Kareler</h2>
-      <div className='squares'>
-        {
-          // Kötü bug!  'KareIdListesi' yerine bir state dilimi kullanmalıyız.
-          // Şöyle diyebiliriz: "aa bu çalışıyor!" Ama kareler bir state diliminden gelmiyorsa,
-          // asla yeni kare ekleyemeyiz, kareleri düzenleyemeyiz ya da silemeyiz. Düzeltin!
-          KareIdListesi.map(id =>
-            <div
-              id={id}
-              key={id}
-              className={`square ${ClassAdiAl(id)}`}
-              onClick={() => AktifEt(id)}
-            >
-            </div>
-          )
-        }
+      <div className="squares">
+        {kareler.map((id) => (
+          <div
+            id={id}
+            key={id}
+            className={`square ${ClassAdiAl(id)}`}
+            onClick={() => AktifEt(id)}
+          ></div>
+        ))}
       </div>
+      <button onClick={yeniKareEkle}>Yeni Kare Ekle</button>
+      <button onClick={kareSil}>Aktif Kareyi Sil</button>
     </div>
   );
 }
